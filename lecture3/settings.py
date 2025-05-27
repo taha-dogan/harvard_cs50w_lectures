416ff96586be51dc5c0081411bc766cba2fbe49a
Taha <flakysob@gmail.com>
2025-05-28 00:28:38 +0300

    Implement task management features with forms, templates, and styling

--- lecture3/settings.py
+++ lecture3/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "static/"
-
+STATICFILES_DIRS = [BASE_DIR / "tasks/static"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,14 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-tasks = ["check emails", "send email", "set meets"]
+tasks = []
+
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="Add Task Name ", max_length=100)
+
 
 # Create your views here.
 def index(request):
@@ -8,4 +16,13 @@ def index(request):
 
 
 def add(request):
-    return render(request, "add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {"form": form})
+
+    return render(request, "add.html", {"form": NewTaskForm()})
